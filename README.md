52e821dd288ffdf03b553affcef7ac80efbfba97
Guido <guidopercu@gmail.com>
2017-05-01 23:44:30 -0300

    Trying first deploy

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Vehicle Safety API
 
 API to expose [NHTSA NCAP 5 Star Safety Ratings API](https://one.nhtsa.gov/webapi/Default.aspx?SafetyRatings/API/5)
+
+## Installation and usage
+
+```
+composer install
+php -S localhost:8000 -t public
+```
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 // GET http://localhost:8080/vehicles/<MODEL YEAR>/<MANUFACTURER>/<MODEL>
 $app->get('/vehicles/{model_year}/{manufacturer}/{model}', function () use ($app) {
+    $client = new GuzzleHttp\Client();
+    $res = $client->request('GET', 'https://one.nhtsa.gov/webapi/api/SafetyRatings/modelyear/<MODEL YEAR>/make/>MANUFACTURER>/model/<MODEL>?format=json', []);
     return $app->version();
 });
 
@@ -13,5 +15,7 @@ $app->post('/vehicles', function () use ($app) {
 
 // GET http://localhost:8080/vehicles/<MODEL YEAR>/<MANUFACTURER>/<MODEL>?withRating=true
 $app->get('/vehicles/{model_year}/{manufacturer}/{model}', function () use ($app) {
+    $client = new GuzzleHttp\Client();
+    $res = $client->request('GET', 'https://one.nhtsa.gov/webapi/api/SafetyRatings/modelyear/<MODEL YEAR>/make/>MANUFACTURER>/model/<MODEL>?format=json', []);
     return $app->version();
 });
