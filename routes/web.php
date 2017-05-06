1503790b2664b52f56c9d4380e82df7ad6d0841a
Guido <guidopercu@gmail.com>
2017-05-06 01:22:55 -0300

    Change name model_year to year on routes

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@ $app->get('/', function () use ($app) {
     return $app->version();
 });
 
-$app->get('/vehicles/{model_year}/{manufacturer}/{model}', 'VehicleSafetyRatingsController@getAll');
-$app->get('/vehicles/{model_year}/{manufacturer}/{model}?withRating=True', 'VehicleSafetyRatingsController@getAll');
+$app->get('/vehicles/{year}/{manufacturer}/{model}', 'VehicleSafetyRatingsController@getAll');
+$app->get('/vehicles/{year}/{manufacturer}/{model}?withRating=True', 'VehicleSafetyRatingsController@getAll');
 $app->post('/vehicles/', 'VehicleSafetyRatingsController@post');
 
 $app->get('/healthcheck', function () use ($app) {
