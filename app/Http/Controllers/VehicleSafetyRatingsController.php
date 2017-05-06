eb126fca352625a77b608997df7069f8a8c3ef3b
Guido <guidopercu@gmail.com>
2017-05-06 00:25:43 -0300

    Creating getAll method using TDD

--- app/Http/Controllers/VehicleSafetyRatingsController.php
+++ app/Http/Controllers/VehicleSafetyRatingsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Response;
+
 class VehicleSafetyRatingsController extends Controller
 {
     /**
@@ -16,12 +18,22 @@ class VehicleSafetyRatingsController extends Controller
 
     public function getAll()
     {
-
+        $tdd = [
+                "Count" => 4,
+                "Results" => [
+                        ["Description" => "2015 Audi A3 4 DR AWD", "VehicleId" => 9403],
+                        ["Description" => "2015 Audi A3 4 DR FWD", "VehicleId" => 9408],
+                        ["Description" => "2015 Audi A3 C AWD", "VehicleId" => 9405],
+                        ["Description" => "2015 Audi A3 C FWD", "VehicleId" => 9406],
+                ]
+        ];
+        
+        return response()->json($tdd, 200);
     }
 
     public function post()
     {
-
+        return response()->json([], 201);
     }
     
     protected function getAllWithRatings()
