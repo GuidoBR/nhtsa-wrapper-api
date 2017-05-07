9453ce6538584a57abdd5edf02b98f7a26c2a3ef
Guido <guidopercu@gmail.com>
2017-05-07 17:57:04 -0300

    Bootstrap on index page

--- resources/views/index.php
+++ resources/views/index.php
@@ -3,15 +3,21 @@
 <head>
     <meta charset="utf-8">
     <title> NHTSA - API Wrapper </title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 
 <body>
-    <h1>Vehicle Safety API</h1>
-    <p>This is a wrapper to the <a href="https://one.nhtsa.gov/webapi/Default.aspx?SafetyRatings/API/5">NHTSA NCAP 5 Star Safety Ratings API</a></p>
+  <div class="container">
 
-    <ul>
-        <li><a href="http://docs.vehiclesafetyapi.apiary.io/" title="Documentation on Apiary">Documentation</a></li>
-        <li><a href="https://github.com/GuidoBR/nhtsa-wrapper-api" title="Code on Github">Github</a></li>
-    <ul>
+      <div class="starter-template">
+        <h1>Vehicle Safety API</h1>
+        <p class="lead">This is a wrapper to the <a href="https://one.nhtsa.gov/webapi/Default.aspx?SafetyRatings/API/5">NHTSA NCAP 5 Star Safety Ratings API</a></p>
+        <ul>
+            <li><a href="http://docs.vehiclesafetyapi.apiary.io/" title="Documentation on Apiary">Documentation</a></li>
+            <li><a href="https://github.com/GuidoBR/nhtsa-wrapper-api" title="Code on Github">Github</a></li>
+        <ul>
+      </div>
+
+    </div>
 </body>
 <html>
