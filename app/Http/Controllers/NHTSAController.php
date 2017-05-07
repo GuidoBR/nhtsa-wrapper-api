f3466ae1a0787c99bc9ef12becff737fdfd57d37
Guido <guidopercu@gmail.com>
2017-05-07 15:35:09 -0300

    Auto ident

--- app/Http/Controllers/NHTSAController.php
+++ app/Http/Controllers/NHTSAController.php
@@ -42,32 +42,32 @@ class NhtsaController extends Controller
             print($e);
             return response()->json(["Count" => 0, "Results" => []], 500);
         }
-    
+
     }
 
     protected function getOverallRating($response)
     {
-            $json = json_decode($response->getBody(), true);
-            return $json["Results"][0]["OverallRating"];
-    
+        $json = json_decode($response->getBody(), true);
+        return $json["Results"][0]["OverallRating"];
+
     }
 
     protected function createResponse($response, $method)
     {
-            $apiResponse = [];
-            $json = json_decode($response->getBody(), true);
-            if ($response->getStatusCode() == 200 && $json['Count'] != 0) {
-                    $apiResponse["Count"] = $json["Count"];
-                    $apiResponse["Results"] = [];
-                    foreach ($json["Results"] as $result) {
-                            $apiResponse["Results"][] = ["Description" => $result["VehicleDescription"], "VehicleId" => $result["VehicleId"]];
-                    }
-                    if ($method == "GET") {
-                            return  response()->json($apiResponse, 200);
-                    }
-                    return  response()->json($apiResponse, 201);
+        $apiResponse = [];
+        $json = json_decode($response->getBody(), true);
+        if ($response->getStatusCode() == 200 && $json['Count'] != 0) {
+            $apiResponse["Count"] = $json["Count"];
+            $apiResponse["Results"] = [];
+            foreach ($json["Results"] as $result) {
+                $apiResponse["Results"][] = ["Description" => $result["VehicleDescription"], "VehicleId" => $result["VehicleId"]];
             }
+            if ($method == "GET") {
+                return  response()->json($apiResponse, 200);
+            }
+            return  response()->json($apiResponse, 201);
+        }
 
-            return response()->json(["Count" => 0, "Results" => []], 404);
+        return response()->json(["Count" => 0, "Results" => []], 404);
     }
 }
